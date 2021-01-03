6e467dd4fab065d19a3d8ba205d516d236c6ab93
neha_turakhia <nehaturakhia95@gmail.com>
2021-01-03 12:14:34 +0530

    Implemented using character arrays causing a significant improvement in space complexity for an exponentially long (or in order of 10^3) input string.

--- ArrayString/IsUnique.java
+++ ArrayString/IsUnique.java
@@ -25,14 +25,31 @@ public class IsUnique {
         }*/
 
         /*Solution 2: Using HashSets*/
-        HashSet<Character> hs = new HashSet<>();
+        /*HashSet<Character> hs = new HashSet<>();
         for(int i=0;i<s.length();++i){
             char c = s.charAt(i);
             if(hs.contains(c)) {
                 return false;
             }
             hs.add(c);
+        }*/
+
+        /*Solution 3: Using character arrays*/
+        /**
+         * Assumption - that the input will only be 7 bit ASCII (128 chars)
+         * Reason - if the unique string is exponentially long then the hashmap/hashset would be a very expensive choice for storage.
+         * For a HashSet/HashMap the Space Complexity ~ O(n)  hashset/hashmap
+         * In case of a char array, Space Complexity ~ O(1) since it is either 128 or 256 depending on whether it is 128/256 ASCII
+         **/
+        boolean[] ascii = new boolean[128];
+        for(int i=0;i<s.length();++i){
+            char c = s.charAt(i);
+            if(ascii[c]){
+                return false;
+            }
+            ascii[c]= true;
         }
+
         return true;
     }
 }
