37d82511ab2fd6735f382c4dc541389c44c39a0a
neha_turakhia <nehaturakhia95@gmail.com>
2021-01-03 21:56:18 +0530

    Time Complexity - O(n) Space Complexity ~ O(n) -- since we form a new compressed string which may be of the same length or higher

---
+++ ArrayString/StringCompression.java
@@ -0,0 +1,32 @@
+package ArrayString;
+
+import java.util.Scanner;
+
+public class StringCompression {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String str = scan.nextLine();
+        String compressed_string = compress(str);
+        System.out.println(compressed_string);
+    }
+
+    private static String compress(String original){
+        StringBuilder sb = new StringBuilder();
+        char prev = original.charAt(0);
+        int count = 1;
+        for(int i=1;i<original.length();++i){
+            if(original.charAt(i) == prev) {
+                count++;
+            }else{
+                sb.append(prev);
+                sb.append(count);
+                count = 1;
+            }
+            prev = original.charAt(i);
+        }
+
+        sb.append(prev);
+        sb.append(count);
+        return sb.length() >= original.length() ? original : sb.toString();
+    }
+}
