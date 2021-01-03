92289ea18fdff352b5096c38aaf9ab985f29f6d6
neha_turakhia <nehaturakhia95@gmail.com>
2021-01-03 13:57:20 +0530

    Implemented a Hashmap to store the frequency of every character. A palindrome will have all characters with even frequency or all with even frequency + 1 char with odd frequency. Once the hashmap is constructed , check for multiple chars with odd frequency and return the result.
    Time complexity - O(n + k) ~ O(n) where k is the size of the hashmap
    Space Complexity - O(k) where k is the number of unique characters(or keys) in the hashmap

---
+++ ArrayString/PalindromePermutation.java
@@ -0,0 +1,39 @@
+package ArrayString;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Scanner;
+
+public class PalindromePermutation {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+        boolean isPalindromePerm = checkForPalindromePerm(input);
+        System.out.println(isPalindromePerm);
+    }
+
+    private static boolean checkForPalindromePerm(String s){
+        s = s.toLowerCase();
+        HashMap<Character,Integer> hm = new HashMap<>();
+        for(int i=0;i<s.length();++i) {
+            char c = s.charAt(i);
+            if(c != ' ') {
+                hm.put(c, hm.getOrDefault(c, 0) + 1);
+            }
+        }
+
+        boolean isOdd = false;
+        Iterator i = hm.keySet().iterator();
+        while(i.hasNext()){
+            char c = (char) i.next();
+            if(hm.get(c) % 2 !=0) {
+                if(isOdd) {
+                    return false;
+                }
+                isOdd=true;
+            }
+        }
+        return true;
+    }
+}
+
