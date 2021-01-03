eb1742e20b61bc3f189bd2e09fcddeda4e1fd7b7
neha_turakhia <nehaturakhia95@gmail.com>
2021-01-03 12:20:22 +0530

    Implemented using character arrays causing a significant improvement in space complexity for an exponentially long (or in order of 10^3) input string.

--- ArrayString/CheckPermutation.java
+++ ArrayString/CheckPermutation.java
@@ -18,8 +18,8 @@ public class CheckPermutation {
         if(s1 == null || s2 == null) return false;
         if(s1.length() != s2.length()) return false;
 
-        boolean isPermutation = true;
-        HashMap<Character,Integer> hm = new HashMap<>();
+        /*Solution 1: Using HashMaps*/
+        /*HashMap<Character,Integer> hm = new HashMap<>();
         for(int i=0;i< s1.length();++i){
             char c = s1.charAt(i);
             hm.put(c,hm.getOrDefault(c,0)+1);
@@ -32,8 +32,29 @@ public class CheckPermutation {
                break;
             }
             hm.put(c,hm.getOrDefault(c,0)-1);
+        }*/
+
+        /*Solution 2: Using character arrays*/
+
+        /**
+         * Assumption - that the input will only be 7 bit ASCII (128 chars)
+         * Reason - if the strings (assuming are of equal length) are exponentially long then the hashmap would be a very expensive choice for storage.
+         * For a HashMap the Space Complexity ~ O(n)
+         * In case of a char array, Space Complexity ~ O(1) since it is either 128 or 256 depending on whether it is 128/256 ASCII
+         **/
+        int[] ascii = new int[128];
+        for(int i=0;i< s1.length();++i) {
+            char c = s1.charAt(i);
+            ascii[c]++;
         }
 
-        return isPermutation;
+        for(int i=0;i< s2.length();++i){
+            char c = s2.charAt(i);
+            ascii[c]--;
+            if(ascii[c] == -1){
+               return false;
+            }
+        }
+        return true;
     }
 }
