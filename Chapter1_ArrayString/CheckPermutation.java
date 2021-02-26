3873b473d894c6628cf5ba19665447c698fae88f
neha_turakhia <nehaturakhia95@gmail.com>
2021-02-27 00:09:01 +0530

    Complexity analysis done for 1.1 and 1.2

--- Chapter1_ArrayString/CheckPermutation.java
+++ Chapter1_ArrayString/CheckPermutation.java
@@ -5,6 +5,28 @@ import java.util.Scanner;
 public class CheckPermutation {
     /**
      * CTCI 1.2 Check Permutation : Given two strings, write a method to decide if one is a permutation of the other.
+     *
+     * Solution1 : Using Hashmaps
+     * Approach : Store string1 in the hashmap with a char count as the value and
+     *            decrement the value for each char in the map while traversing string2.
+     *
+     * Disadvantage : if the 2 string are equal and exponentially long then it is a very costly operation.
+     *
+     * Time Complexity : O(N1+N2+1) ~ O(N1+N2) - if the two strings are of different length, O(N) - strings are of same length.
+     * Note : HashMap lookup is of constant time lookup , hence we have added 1.
+     *
+     * Space Complexity: O(N1) ~ O(N) for the hashmap (to store string1)
+     *
+     *
+     *
+     * Solution 2: Using character arrays
+     * Approach : We have assumed that the input will only be 7 bit ASCII (128 chars). We create a char array of length 128 and
+     *            increment the count upon traversing string1. Then we traverse string2 and decrement the count for each of the chars
+     *            in the array. If at any point the value in the char array is -1, this means that the strings are not a permutation
+     *            of one another.
+     *
+     * Time Complexity : O(N1+N2) (if the strings are of unequal length) , O(N) (for equal length strings)
+     * Space Complexity : O(1) since it is either 128 or 256 depending on whether it is 128/256 ASCII
      **/
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
@@ -38,12 +60,6 @@ public class CheckPermutation {
 
         /*Solution 2: Using character arrays*/
 
-        /**
-         * Assumption - that the input will only be 7 bit ASCII (128 chars)
-         * Reason - if the strings (assuming are of equal length) are exponentially long then the hashmap would be a very expensive choice for storage.
-         * For a HashMap the Space Complexity ~ O(n)
-         * In case of a char array, Space Complexity ~ O(1) since it is either 128 or 256 depending on whether it is 128/256 ASCII
-         **/
         int[] ascii = new int[128];
         for(int i=0;i< s1.length();++i) {
             char c = s1.charAt(i);
--- Chapter1_ArrayString/IsUnique.java
+++ Chapter1_ArrayString/IsUnique.java
@@ -6,6 +6,36 @@ public class IsUnique {
     /**
      * CTCI 1.1 IsUnique : Implement an algorithm to determine if a string has all unique characters.
      *                     What if you cannot use additional data structures?
+     *
+     *
+     * Solution 1: Using HashMaps
+     * Approach : Store every char and its count as a key-value pair in a hashmap.
+     *            Traverse the string, if any char already exists return false.
+     *
+     *  Time Complexity - O(N)
+     *  Space Complexity - O(N) - for the hashmap
+     *
+     *
+     * Solution 2: Using HashSets
+     * Approach : it is not necessary to persist the count of the character since we need to determine
+     *            if every character exists only once. We can use hashsets for this , though this will not reduce
+     *            the space complexity but it will reduce the space used by the algorithm by 1/2 since we dont store
+     *            a key-value pair.
+     * Note - hashset is used instead of a list since a lookup operation in hashset is of constant time ( O(1) )
+     *        whereas it is O(N) for a list.
+     *
+     * Time Complexity :  O(N)
+     * Space Complexity - O(N) - for the hashset
+     *
+     *
+     * Solution 3: Using character arrays
+     * Approach : It is assumed that the input will only be 7 bit ASCII (128 chars). We create a char array of length 128 and
+     *            increment the count upon traversing the string. if any char has count greater than 1 already the string is not unique
+     * Reason - if the unique string is exponentially long then the hashmap/hashset would be a very expensive choice for storage.
+     *
+     * Time Complexity : O(N)
+     * Space Complexity ~ O(1) since it is either 128 or 256 depending on whether it is 128/256 ASCII
+     *
      * **/
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
@@ -37,12 +67,6 @@ public class IsUnique {
         }*/
 
         /*Solution 3: Using character arrays*/
-        /**
-         * Assumption - that the input will only be 7 bit ASCII (128 chars)
-         * Reason - if the unique string is exponentially long then the hashmap/hashset would be a very expensive choice for storage.
-         * For a HashSet/HashMap the Space Complexity ~ O(n)  hashset/hashmap
-         * In case of a char array, Space Complexity ~ O(1) since it is either 128 or 256 depending on whether it is 128/256 ASCII
-         **/
         boolean[] ascii = new boolean[128];
         for(int i=0;i<s.length();++i){
             char c = s.charAt(i);
